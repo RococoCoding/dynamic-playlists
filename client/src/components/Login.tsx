50b9bbabaf027f12b83df54b4df30f10718626a4
RococoCoding <sinsofangels@gmail.com>
2023-10-25 14:38:58 -0400

    cleanup Login component

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,6 +1,5 @@
 import { useUserContext } from "../contexts/user";
 import { ENVIRONMENTS, REACT_APP_ENV } from "../constants";
-import { useSnackbarContext } from "../contexts/snackbar";
 import { getDpUser } from "../utils/users/dp";
 import { getSpotifyUser } from "../utils/users/spotify";
 import useSpotifyApi from "../utils/useSpotifyApi";
@@ -8,43 +7,36 @@ import { useNavigate } from "react-router-dom";
 import getAuthorizeSpotifyArgs from "../utils/authorizeSpotify";
 
 function Login() {
-  const { userId } = useUserContext();
-  const { setErrorSnackbar } = useSnackbarContext();
+  const { userId, setUserIdContext } = useUserContext();
   const { callSpotifyApi } = useSpotifyApi();
-  const { setUserIdContext } = useUserContext();
   const navigate = useNavigate();
 
   const startLogin = async () => {
-    try {
-      if (userId) {
-        navigate('/home/' + userId);
-      } else {
-        try {
-          const spotifyUser = await getSpotifyUser(callSpotifyApi);
-          if (spotifyUser) {
-            const { id: spotifyUserId } = spotifyUser;
-            // double-check dp user exists too
-            const dpUser = await getDpUser(spotifyUserId);
-            if (dpUser) {
-              setUserIdContext(spotifyUserId);
-              navigate('/home/' + spotifyUserId);
-            } else {
-              throw new Error(`Matching Dp user does not exist for Spotify user ${spotifyUserId}`);
-            }
+    if (userId) {
+      // if userId is in state, we should already be logged in
+      navigate('/home/' + userId);
+    } else {
+      try {
+        // otherwise, attempt to get spotify user, which will trigger token refresh / authorization flow
+        const spotifyUser = await getSpotifyUser(callSpotifyApi);
+        if (spotifyUser) {
+          const { id: spotifyUserId } = spotifyUser;
+          // double-check dp user exists too
+          const dpUser = await getDpUser(spotifyUserId);
+          if (dpUser) {
+            setUserIdContext(spotifyUserId);
+            navigate('/home/' + spotifyUserId);
+          } else {
+            throw new Error(`Matching Dp user does not exist for Spotify user ${spotifyUserId}`);
           }
-        } catch (e: any) {
-          if (REACT_APP_ENV === 'development') {
-            console.log('error getting spotify user', e);
-          }
-          const args = await getAuthorizeSpotifyArgs();
-          window.location.href = 'https://accounts.spotify.com/authorize?' + args;
         }
+      } catch (e: any) {
+        if (REACT_APP_ENV === ENVIRONMENTS.development) {
+          console.log('error getting spotify user', e);
+        }
+        const args = await getAuthorizeSpotifyArgs();
+        window.location.href = 'https://accounts.spotify.com/authorize?' + args;
       }
-    } catch (e: any) {
-      if (REACT_APP_ENV === ENVIRONMENTS.development) {
-        console.log('Error logging in: ', e);
-      }
-      setErrorSnackbar('Error logging in. Please refresh the page and try again.');
     }
   }
 
